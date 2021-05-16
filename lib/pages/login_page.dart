fede932226a1ce6eb59b8cf8e64d5170f45e9729
Suvam Basak <suvambasak2008@gmail.com>
2021-05-16 16:12:39 +0530

    login layout

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,17 +5,66 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
+
+      // column
       child: Column(
         children: [
+          // keep space
+          SizedBox(
+            height: 30.0,
+          ),
+
+          // Image show
           Image.asset(
             "assets/images/login_img.png",
             fit: BoxFit.cover,
           ),
+
+          // Text
           Text(
             "Login Here",
             style: TextStyle(
               fontSize: 30.0,
               fontWeight: FontWeight.bold,
+            ),
+          ),
+
+          // Login username password
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+
+            // Username pwd
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Username",
+                    hintText: "Enter Username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    hintText: "Enter Password",
+                  ),
+                ),
+
+                // space
+                SizedBox(
+                  height: 20.0,
+                ),
+
+                // button
+                ElevatedButton(
+                  onPressed: () {
+                    print('login pressed');
+                  },
+                  child: Text('Login'),
+                  style: TextButton.styleFrom(),
+                ),
+              ],
             ),
           )
         ],
