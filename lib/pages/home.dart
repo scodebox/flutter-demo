3538252a27d303237693b0906b8845649e2a45ae
Suvam Basak <suvambasak2008@gmail.com>
2021-05-18 23:10:05 +0530

    load json file and parse

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:proj_1/models/catalog.dart';
 import 'package:proj_1/wigets/item_widget.dart';
 import '../wigets/drawer.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    // TODO: implement initState ::::: Get the data and pass to the build
+    super.initState();
+
+    // get data
+    loadData();
+  }
+
+  // may take time
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    print(decodedData["products"]);
+  }
+
   @override
   Widget build(BuildContext context) {
     // Test
