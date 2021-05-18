89690b418df7a80cd961689f7ce9a9a4c055048e
Suvam Basak <suvambasak2008@gmail.com>
2021-05-18 22:06:22 +0530

    Product ListView (recyclerview)

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:proj_1/models/catalog.dart';
+import 'package:proj_1/wigets/item_widget.dart';
 import '../wigets/drawer.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Test
+    final dummyList = List.generate(50, (index) => CatalogModels.items[0]);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
@@ -13,13 +18,25 @@ class Home extends StatelessWidget {
         // elevation: 0.0,
         // iconTheme: IconThemeData(color: Colors.black),
       ),
-      body: Container(
-        child: Center(
-            child: Text(
-          'Hello world!',
-          style: TextStyle(color: Colors.black),
-        )),
+
+      // like recyclerview
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          // itemCount: CatalogModels.items.length,
+
+          // Test
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            // return ItemWidgets(item: CatalogModels.items[index]);
+
+            // Test
+            return ItemWidgets(item: dummyList[index]);
+          },
+        ),
       ),
+
+      // Side drawer
       drawer: MyDrawer(),
     );
   }
