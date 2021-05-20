d7888ab533da3fc9ea1007ea1f41d42543c14286
Suvam Basak <suvambasak2008@gmail.com>
2021-05-20 13:56:06 +0530

    Cart design

--- lib/pages/cart.dart
+++ lib/pages/cart.dart
@@ -10,6 +10,54 @@ class Cart extends StatelessWidget {
         title: "Cart".text.color(context.accentColor).make(),
       ),
       backgroundColor: context.canvasColor,
+      body: Column(
+        children: [
+          CartList().p32().expand(),
+          Divider(),
+          CardTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class CardTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$${9999}".text.xl5.color(context.accentColor).make(),
+          ElevatedButton(
+            onPressed: () {},
+            child: "Buy".text.white.make(),
+          ).w24(context)
+        ],
+      ),
+    );
+  }
+}
+
+class CartList extends StatefulWidget {
+  @override
+  _CartListState createState() => _CartListState();
+}
+
+class _CartListState extends State<CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        title: "Item 1".text.make(),
+        leading: Icon(Icons.done),
+        trailing: IconButton(
+          icon: Icon(Icons.remove_circle_outline),
+          onPressed: () {},
+        ),
+      ),
     );
   }
 }
